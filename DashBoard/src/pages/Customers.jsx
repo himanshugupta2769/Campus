bed33d97c418ff2029b589a7ae4c9f1a9325bf9e
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-04-07 15:25:53 +0530

    Added add Student button

--- DashBoard/src/pages/Customers.jsx
+++ DashBoard/src/pages/Customers.jsx
@@ -11,8 +11,194 @@ const Customers = () => {
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Customerssss" />
-      <button>asdfa</button>
+      <div className="flex justify-between">
+        <Header title="Student" />
+
+        <button
+          type="button"
+          // eslint-disable-next-line max-len
+          className="inline-block bg-[#03c9d7]  mb-6 rounded-xl font-bold px-8  pb-2 pt-2.5 text-sm uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-[#03c9d7] hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-[#15c6d3] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+          data-te-toggle="modal"
+          data-te-target="#exampleModal"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+        >
+          Add Student
+        </button>
+
+        <div
+          data-te-modal-init
+          className="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div
+            data-te-modal-dialog-ref
+            className="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]"
+          >
+            <div
+              className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600 mt-[200px]"
+            >
+              <div
+                className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 "
+              >
+
+                <h5
+                  className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+                  id="exampleModalLabel"
+                >
+                  User
+                </h5>
+
+                <button
+                  type="button"
+                  className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                  data-te-modal-dismiss
+                  aria-label="Close"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div className="relative flex-auto p-4" data-te-modal-body-ref>
+                <div
+                  className="block max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700"
+                >
+
+                  <form className="space-y-8 divide-y divide-gray-200">
+                    <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+
+                      <div>
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">Personal Information</h3>
+                        <p className="mt-1 max-w-2xl text-sm text-gray-500">Use a permanent address where you can receive mail.</p>
+                      </div>
+                      <div className="space-y-6 sm:space-y-5">
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="first-name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> First name </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="max-w-lg block w-full p-2 border-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                          </div>
+                        </div>
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="last-name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Last name </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <input type="text" name="last-name" id="last-name" autoComplete="family-name" className="max-w-lg block w-full shadow-sm  p-2 border-2 focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                          </div>
+                        </div>
+
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="email" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Email address </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <input id="email" name="email" type="email" autoComplete="email" className="block max-w-lg w-full shadow-sm focus:ring-indigo-500 p-2 border-2 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md" />
+                          </div>
+                        </div>
+
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="country" className=" block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Country </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <select id="country" name="country" autoComplete="country-name" className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full p-2 border-2 shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                              <option>United States</option>
+                              <option>Canada</option>
+                              <option>Mexico</option>
+                            </select>
+                          </div>
+                        </div>
+
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="street-address" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Street address </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <input type="text" name="street-address" id="street-address" autoComplete="street-address" className="block max-w-lg w-full shadow-sm p-2 border-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md" />
+                          </div>
+                        </div>
+
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="city" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> City </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <input type="text" name="city" id="city" autoComplete="address-level2" className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 p-2 border-2 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                          </div>
+                        </div>
+
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="region" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> State / Province </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <input type="text" name="region" id="region" autoComplete="address-level1" className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 p-2 border-2 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                          </div>
+                        </div>
+
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> ZIP / Postal code </label>
+                          <div className="mt-1 sm:mt-0 sm:col-span-2">
+                            <input type="text" name="postal-code" id="postal-code" autoComplete="postal-code" className="max-w-lg block w-full shadow-sm p-2 border-2 focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                      <label htmlFor="cover-photo" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Photo</label>
+                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                        <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                          <div className="space-y-1 text-center">
+                            <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                              <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                            <div className="flex text-sm text-gray-600">
+                              <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                <span>Upload Photo</span>
+                                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                              </label>
+                              <p className="pl-1">or drag and drop</p>
+                            </div>
+                            <p className="text-xs text-gray-500">PNG, JPG up to 10MB</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+
+              <div
+                className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50"
+              >
+                <button
+                  type="button"
+                  className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                  data-te-modal-dismiss
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  // eslint-disable-next-line max-len
+                  className="ml-1 inline-block rounded bg-[#03c9d7] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <GridComponent
         dataSource={customersData}
         enableHover={false}
